d8a7ba7c25a8ddc66f35e3e8d997c9162de6e406
Rajdeep Khandelwal <khandelwalrajdeep@gmail.com>
2023-06-20 17:16:17 +0530

    express app setup

---
+++ app.js
@@ -0,0 +1,11 @@
+//write express basic setup code this is app.js file
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+    res.json({success: true});
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+})
